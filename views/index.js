8bf4f2c09354e7ec809f78ad3c79127bba072275
salelkarayush <salelkarayush@gmail.com>
2024-07-13 11:49:31 +0530

    .

--- views/index.js
+++ views/index.js
@@ -3,7 +3,7 @@ const createHomepageTemplate = () => /*html*/`
   <html>
     <head>
       <title>My Reading List</title>
-      <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+      <script src="https://unpkg.com/htmx.org@2.0.1" integrity="sha384-QWGpdj554B4ETpJJC9z+ZHJcA/i59TyjxEPXiiUgN2WmTyV5OEZWCD6gQhgkdpB/" crossorigin="anonymous"></script>
       <link rel="stylesheet" href="/styles.css">
     </head>
     <body>
